9b6ba9fac9e7337f28e1c809a82f013679cfc5bd
Faisal Hameed <faisalh374@gmail.com>
2025-05-11 06:05:41 +0500

    update

---
+++ 02_intermidate/list and dict/main.py
@@ -0,0 +1,9 @@
+def main():
+    furits_list: list = ["apple", "banana", "orange", "grape", "kiwi"]
+    print("Fruits List: ", furits_list)
+    print("Fruits List Length: ", len(furits_list))
+    print(furits_list.append("mango"))
+    print("Fruits List After Append: ", furits_list)
+if    __name__ == "__main__":
+    main()
+    
